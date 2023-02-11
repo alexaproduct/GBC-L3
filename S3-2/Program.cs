66b55111188dc8951db6cfba2cc00fcf7ff89361
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-11 16:05:33 +0600

    Task S3-2 completed

---
+++ S3-2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2 - Найти сумму чисел от 1 до А
+
+Console.WriteLine("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+
+int result = 0;
+
+for (int i = 1; i <= A; i++)
+{
+    result = result + i;
+}
+Console.WriteLine(result);
