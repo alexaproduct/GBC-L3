a58044939db68213a757b6f62cc914a359c86d68
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-11 15:58:33 +0600

    Task S3-1 completed

---
+++ S3-1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"{i} x {i} = {i*i}");
+}
+Console.WriteLine();
