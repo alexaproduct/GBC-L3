abac40d5e67959c4b11ceca2057d8166d25db6ab
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 21:48:24 +0600

    Task 4 added

---
+++ S3-4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов и 
+// вывести их на экран
+
+int[] arr = new int[8];
+
+for (int i = 0; i <= 7; i++)
+{
+    arr[i] = new Random().Next(1, 100);
+    Console.WriteLine(arr[i]);
+}
