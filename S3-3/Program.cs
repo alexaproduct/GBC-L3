d33574d1d37fd20347ae5e9933327cd95bec181d
Alexandra Zotova <alexa@Alexandras-MacBook-Pro.local>
2023-02-12 20:21:33 +0600

    Task 3 solution added

---
+++ S3-3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определить количество цифр в числе
+
+Console.WriteLine("Введите число:");
+int N = int.Parse(Console.ReadLine());
+
+int i = 0;
+
+while (N>0)
+{
+    i++;
+    N=N/10;
+}
+Console.WriteLine("Количество цифр в числе: " + i);
